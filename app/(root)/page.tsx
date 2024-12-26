01426f6d955425a0d73b02049f6f8d57f8f02e88
Bashir <bashir@DJP-M2-Mac-mini.local>
2024-12-26 16:48:14 +0900

    the un used button commented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 import ProductList from "@/components/shared/product/product-list";
 import { getLatestProducts } from "@/lib/actions/product.actions";
 
-import { Button } from "@/components/ui/button";
+//import { Button } from "@/components/ui/button";
 const HomePage = async () => {
   const latestProducts =  await getLatestProducts();
   return (
