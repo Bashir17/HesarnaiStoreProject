e13af1cb22fc7a2870ab33d6f707d3d2ec87510c
Bashir <bashir@DJP-M2-Mac-mini.local>
2024-12-26 11:48:50 +0900

    list sample prodiuct added also the limitation applied to show the number of product

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,13 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 import { Button } from "@/components/ui/button";
-const Homepage = () => {
-  return <>Latest Products</>
-}
- 
-export default Homepage;
+const HomePage = () => {
+  return (
+    <div className='space-y-8'>
+      <h2 className='h2-bold'>Latest Products</h2>
+      <ProductList title='Newest Arrivals' data={sampleData.products} limit={4}/>
+    </div>
+  );
+};
+export default HomePage;
