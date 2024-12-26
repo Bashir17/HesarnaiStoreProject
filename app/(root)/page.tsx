3b11f66d59be1308032f6dc6be251cc956e86d42
Bashir <bashir@DJP-M2-Mac-mini.local>
2024-12-26 13:41:43 +0900

    Product card component added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 const HomePage = () => {
   return (
     <div className='space-y-8'>
-      <h2 className='h2-bold'>Latest Products</h2>
+   {/*    <h2 className='h2-bold'>Latest Products</h2> */}
       <ProductList title='Newest Arrivals' data={sampleData.products} limit={4}/>
     </div>
   );
