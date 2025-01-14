c0ced90973e1bcb6315017ee0fa62f400c169be1
Bashir <bashir@DJP-M2-Mac-mini.local>
2025-01-14 10:35:51 +0900

    persisst sessions cad added

--- auth.ts
+++ auth.ts
@@ -74,6 +74,7 @@ export const config = {
     async jwt({ token, user, trigger, session }: any) {
       // Assign user fields to token
       if (user) {
+        token.id = user.id;
         token.role = user.role;
 
         // If user has no name, use email as their default name
@@ -86,6 +87,29 @@ export const config = {
             data: { name: token.name },
           });
         }
+        if (trigger === 'signIn' || trigger === 'signUp') {
+          const cookiesObject = await cookies();
+          const sessionCartId = cookiesObject.get('sessionCartId')?.value;
+
+          if (sessionCartId) {
+            const sessionCart = await prisma.cart.findFirst({
+              where: { sessionCartId },
+            });
+
+            if (sessionCart) {
+              // Delete current user cart
+              await prisma.cart.deleteMany({
+                where: { userId: user.id },
+              });
+
+              // Assign new cart
+              await prisma.cart.update({
+                where: { id: sessionCart.id },
+                data: { userId: user.id },
+              });
+            }
+          }
+        }
       }
 
       // Handle session updates (e.g., name change)
@@ -95,6 +119,7 @@ export const config = {
 
       return token;
     },
+    
     authorized({ request, auth }: any) {
       // Check for cart cookie
       if (!request.cookies.get('sessionCartId')) {
@@ -122,5 +147,6 @@ export const config = {
     },
   },
 } satisfies NextAuthConfig;
+
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
