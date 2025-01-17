5d89deec5e7ff5b57d6e9c1db677f1826c14da3e
Bashir <bashir@DJP-M2-Mac-mini.local>
2025-01-17 14:43:10 +0900

    submit button added

--- components/shared/admin/product-form.tsx
+++ components/shared/admin/product-form.tsx
@@ -84,16 +84,16 @@ const ProductForm = ({
       }
     }
   };
-  const images = form.watch('images');
-  const isFeatured = form.watch('isFeatured');
-const banner = form.watch('banner');
+  const images = form.watch("images");
+  const isFeatured = form.watch("isFeatured");
+  const banner = form.watch("banner");
   return (
     <Form {...form}>
-      <form 
-         method="post"
-         onSubmit={form.handleSubmit(onSubmit)}
-         className="space-y-8"
-       >
+      <form
+        method="post"
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+      >
         <div className="flex flex-col gap-5 md:flex-row">
           {/* Name */}
           <FormField
@@ -101,7 +101,6 @@ const banner = form.watch('banner');
             name="name"
             render={({
               field,
-
             }: {
               field: ControllerRenderProps<
                 z.infer<typeof insertProductSchema>,
@@ -253,92 +252,92 @@ const banner = form.watch('banner');
         <div className="upload-field flex flex-col gap-5 md:flex-row">
           {/* Images */}
           <FormField
-  control={form.control}
-  name='images'
-  render={() => (
-    <FormItem className='w-full'>
-      <FormLabel>Images</FormLabel>
-      <Card>
-        <CardContent className='space-y-2 mt-2 min-h-48'>
-          <div className='flex-start space-x-2'>
-            {images.map((image: string) => (
-              <Image
-                key={image}
-                src={image}
-                alt='product image'
-                className='w-20 h-20 object-cover object-center rounded-sm'
-                width={100}
-                height={100}
-              />
-            ))}
-            <FormControl>
-              <UploadButton
-                endpoint='imageUploader'
-                onClientUploadComplete={(res: { url: string }[]) => {
-                  form.setValue('images', [...images, res[0].url]);
-                }}
-                onUploadError={(error: Error) => {
-                  toast({
-                    variant: 'destructive',
-                    description: `ERROR! ${error.message}`,
-                  });
-                }}
-              />
-            </FormControl>
-          </div>
-        </CardContent>
-      </Card>
-      <FormMessage />
-    </FormItem>
-  )}
-/>
+            control={form.control}
+            name="images"
+            render={() => (
+              <FormItem className="w-full">
+                <FormLabel>Images</FormLabel>
+                <Card>
+                  <CardContent className="space-y-2 mt-2 min-h-48">
+                    <div className="flex-start space-x-2">
+                      {images.map((image: string) => (
+                        <Image
+                          key={image}
+                          src={image}
+                          alt="product image"
+                          className="w-20 h-20 object-cover object-center rounded-sm"
+                          width={100}
+                          height={100}
+                        />
+                      ))}
+                      <FormControl>
+                        <UploadButton
+                          endpoint="imageUploader"
+                          onClientUploadComplete={(res: { url: string }[]) => {
+                            form.setValue("images", [...images, res[0].url]);
+                          }}
+                          onUploadError={(error: Error) => {
+                            toast({
+                              variant: "destructive",
+                              description: `ERROR! ${error.message}`,
+                            });
+                          }}
+                        />
+                      </FormControl>
+                    </div>
+                  </CardContent>
+                </Card>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
         </div>
         <div className="upload-field">
           {/* Is Featured */}
           Featured Product
-  <Card>
-    <CardContent className='space-y-2 mt-2  '>
-      <FormField
-        control={form.control}
-        name='isFeatured'
-        render={({ field }) => (
-          <FormItem className='space-x-2 items-center'>
-            <FormControl>
-              <Checkbox
-                checked={field.value}
-                onCheckedChange={field.onChange}
+          <Card>
+            <CardContent className="space-y-2 mt-2  ">
+              <FormField
+                control={form.control}
+                name="isFeatured"
+                render={({ field }) => (
+                  <FormItem className="space-x-2 items-center">
+                    <FormControl>
+                      <Checkbox
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                    <FormLabel>Is Featured?</FormLabel>
+                  </FormItem>
+                )}
               />
-            </FormControl>
-            <FormLabel>Is Featured?</FormLabel>
-          </FormItem>
-        )}
-      />
-      {isFeatured && banner && (
-        <Image
-          src={banner}
-          alt='banner image'
-          className=' w-full object-cover object-center rounded-sm'
-          width={1920}
-          height={680}
-        />
-      )}
-      {isFeatured && !banner && (
-        <UploadButton
-          endpoint='imageUploader'
-          onClientUploadComplete={(res: { url: string }[]) => {
-            form.setValue('banner', res[0].url);
-          }}
-          onUploadError={(error: Error) => {
-            toast({
-              variant: 'destructive',
-              description: `ERROR! ${error.message}`,
-            });
-          }}
-        />
-      )}
-    </CardContent>
-  </Card>
-          </div>
+              {isFeatured && banner && (
+                <Image
+                  src={banner}
+                  alt="banner image"
+                  className=" w-full object-cover object-center rounded-sm"
+                  width={1920}
+                  height={680}
+                />
+              )}
+              {isFeatured && !banner && (
+                <UploadButton
+                  endpoint="imageUploader"
+                  onClientUploadComplete={(res: { url: string }[]) => {
+                    form.setValue("banner", res[0].url);
+                  }}
+                  onUploadError={(error: Error) => {
+                    toast({
+                      variant: "destructive",
+                      description: `ERROR! ${error.message}`,
+                    });
+                  }}
+                />
+              )}
+            </CardContent>
+          </Card>
+        </div>
         <div>
           {/* Description */}
           <FormField
@@ -368,7 +367,14 @@ const banner = form.watch('banner');
         </div>
         <div>
           {/* Submit */}
-       
+          <Button
+            type="submit"
+            size="lg"
+            disabled={form.formState.isSubmitting}
+            className="button col-span-2 w-full"
+          >
+            {form.formState.isSubmitting ? "Submitting" : `${type} Product`}
+          </Button>
         </div>
       </form>
     </Form>
