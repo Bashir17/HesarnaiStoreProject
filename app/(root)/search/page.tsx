cf35b730f0950d7f2f0a7c1b8ca1d7466652cda8
Bashir <bashir@DJP-M2-Mac-mini.local>
2025-01-21 12:21:12 +0900

    search filter, get filter url function

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -38,9 +38,34 @@ const products = await getAllProducts({
     sort,
   });
   
+  // Construct filter url
+const getFilterUrl = ({
+  c,
+  s,
+  p,
+  r,
+  pg,
+}: {
+  c?: string;
+  s?: string;
+  p?: string;
+  r?: string;
+  pg?: string;
+}) => {
+  const params = { q, category, price, rating, sort, page };
+  if (c) params.category = c;
+  if (p) params.price = p;
+  if (r) params.rating = r;
+  if (pg) params.page = pg;
+  if (s) params.sort = s;
+  return `/search?${new URLSearchParams(params).toString()}`;
+};
   return (
     <div className='grid md:grid-cols-5 md:gap-5'>
       <div className='filter-links'>{/* FILTERS */}</div>
+      URL: {
+  getFilterUrl({ c: "Men's Dress Shirts" })
+}
       <div className='md:col-span-4 space-y-4'>
         <div className='grid grid-cols-1 gap-4 md:grid-cols-3'>
           {products!.data.length === 0 && <div>No product found</div>}
@@ -51,7 +76,9 @@ const products = await getAllProducts({
         {products!.totalPages! > 1 && (
           <Pagination page={page} totalPages={products!.totalPages} />
         )}
+        
       </div>
+      
     </div>
   );
   };
